b00322e07c6be6bbe143c0125d5fb23a3430c661
Rohan karankot <rohankarankot@gmail.com>
2023-07-30 17:23:43 +0530

    meta data added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>app with strapi</title>
-        <meta name="description" content="created by rohan" />
+        <meta name="description" content="An E commerce Projct Demostraion created using Next JS and Strapi Headless CMS" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
